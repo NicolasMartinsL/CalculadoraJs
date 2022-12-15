f40701da2a2834ca1761484881e2580331080e01
Nicolas Martins <114949675+NicolasMartinsL@users.noreply.github.com>
2022-12-15 01:07:54 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,24 +1,3 @@
-/*var b1 = document.querySelector("b1");
-var b2 = document.querySelector("b2");
-var b3 = document.querySelector("b3");
-var b4 = document.querySelector("b4");
-var b5 = document.querySelector("b5");
-var b6 = document.querySelector("b6");
-var b7 = document.querySelector("b7");
-var b8 = document.querySelector("b8");
-var b9 = document.querySelector("b9");
-var b0 = document.querySelector("b0");
-var bdiv = document.querySelector("bdiv");
-var bmul = document.querySelector("bmul");
-var bmen = document.querySelector("bmen");
-var bmai = document.querySelector("bmai");
-var bigu = document.querySelector("bigu");
-var bpon = document.querySelector("bpon")
-var blimpar = document.querySelector("blimpar");
-var tela = document.querySelector("h2");
-const numeros = document.querySelectorAll('[id*=b]');
-document.onclick.b1*/
-
 function limpar(){
     document.getElementById('tela').innerHTML = "";
 }
