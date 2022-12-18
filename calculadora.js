737512ca51f4e4085f5583c2dc1f6643215c690d
Nicolas Martins <114949675+NicolasMartinsL@users.noreply.github.com>
2022-12-18 18:41:11 -0300

    Versao2

--- calculadora.js
+++ calculadora.js
@@ -1,16 +1,42 @@
 function limpar(){
-    document.getElementById('tela').innerHTML = "";
-}
-function apagar(){
-    document.getElementById('tela').innerHTML = document.getElementById('tela').innerHTML.slice(0, -1);
-}
-function escrever(num){
-  var numero = document.getElementById('tela').innerHTML;
-  document.getElementById('tela').innerHTML= numero+num;
-}
-function calcular()
-{
-    var resultado = document.getElementById('tela').innerHTML;
-    document.getElementById('tela').innerHTML = eval(resultado);
-    
-}
+	document.getElementById('resultado').value = "";
+  }
+  
+  function apagar(){
+	document.getElementById('resultado').value = document.getElementById('resultado').value.slice(0, -1);
+  }
+  
+  function escrever(num){
+	document.getElementById("resultado").value += num;
+  }
+  
+  function igual(){
+	var res = [];
+	var answer;
+  
+	  if(document.getElementById("resultado").value.includes("+")){
+		res = document.getElementById("resultado").value.split('+');
+		answer = parseFloat(res[0]) + parseFloat(res[1]);
+	  }
+	  else if(document.getElementById("resultado").value.includes("-")){
+		res = document.getElementById("resultado").value.split('-');
+		answer = parseFloat(res[0]) - parseFloat(res[1]);
+	  }
+	  else if(document.getElementById("resultado").value.includes("/")){
+		res = document.getElementById("resultado").value.split('/');
+		answer = parseFloat(res[0]) / parseFloat(res[1]);
+	  }
+	  else if(document.getElementById("resultado").value.includes("*")){
+		res = document.getElementById("resultado").value.split('*');
+		answer = parseFloat(res[0]) * parseFloat(res[1]);
+	  }
+	  else{
+		answer = parseFloat(document.getElementById("resultado").value);
+	  }
+	return answer;
+  }
+  
+  function calcular(){
+	var result = igual();
+	document.getElementById('resultado').value = result; 
+  }
