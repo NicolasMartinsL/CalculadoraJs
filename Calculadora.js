348b916f63415900b94d8c3e2fa77674f72e8277
Nicolas Martins <114949675+NicolasMartinsL@users.noreply.github.com>
2023-01-04 11:01:59 -0300

    Update Calculadora.js

--- Calculadora.js
+++ Calculadora.js
@@ -7,81 +7,69 @@ function apagar(){
 }
   
 function escrever(num){
-	document.getElementById("resultado").value += num;
-}
+	let escrito = document.getElementById("resultado").value;
+	if(escrito[0] === "*"||escrito[0] === "/"||escrito[0] === "+"||escrito[0] === "."){
+		document.getElementById("resultado").value = "Erro!"
+	}
+	if(num == "*" && escrito.slice(-1) == "*"){document.getElementById("resultado").value = "Erro!"}
+	if(num == "/" && escrito.slice(-1) == "/"){document.getElementById("resultado").value = "Erro!"}
+	if(num == "+" && escrito.slice(-1) == "+"){document.getElementById("resultado").value = "Erro!"}
+	if(num == "-" && escrito.slice(-1) == "-"){document.getElementById("resultado").value = "Erro!"}
+	if(num == "." && escrito.slice(-1) == "."){document.getElementById("resultado").value = "Erro!"}
+	document.getElementById("resultado").value += num;}
 function numeros(){
     var operacao = document.getElementById('resultado').value;
-	var numeros = operacao.split(/[/*+-]/);
-
-    if(operacao[0] === "*"||operacao[0] === "/"||operacao[0] === "-"||operacao[0] === "+"||operacao[0] === ""){
-		alert("Erro!");
+	let numerosString = operacao.split(/[/*+-]/);
+	var numeros = [];
+	for(i=0;i<numerosString.length;i++){
+		numeros[i] = parseFloat(numerosString[i]);
 	}
-
     return numeros;
 }
 function operadores(){
     var operacao = document.getElementById('resultado').value;
-	var op = operacao.split(/[0-9]/);
-    var operadores = op.filter(x=>x!="");
+	var op = operacao.split(/[0-9.]/);
+    let operadores = op.filter(x=>x!="");
     return operadores;
 }
-function calcularMulDiv(){
-    var operadoresMulDiv = operadores();
-    var numerosMulDiv = numeros();
-    var retorno = [];
-    var o = -1;
+function igual(){
+    var resultadoFinal = calculo();
+	document.getElementById('resultado').value = resultadoFinal;
+}
+function calculo(){
+	var operador = operadores();
+    var numero = numeros();
 
-    for(var i = 0; i <= operadoresMulDiv.length+1;i++){
-        if(operadoresMulDiv[i] === "*"){
-            let indice = operadoresMulDiv.indexOf("*");
-            let resposta = numerosMulDiv[indice] * numerosMulDiv[indice+1];
-            numerosMulDiv.splice(indice,2,String(resposta));
-            operadoresMulDiv.splice(indice,1);
+    for(var i = 0; i <= numero.length;i++){
+        if(operador[i] === "*"){
+            let resposta = numero[i] * numero[i+1];
+            numero.splice(i,2,resposta);
+            operador.splice(i,1);
             i=-1;
         }
-        if(operadoresMulDiv[i] === "/"){
-            let indice = operadoresMulDiv.indexOf("/");
-            let resposta = numerosMulDiv[indice] / numerosMulDiv[indice+1];
-            numerosMulDiv.splice(indice,2,String(resposta));
-            operadoresMulDiv.splice(indice,1);
+        if(operador[i] === "/"){
+            let resposta = numero[i] / numero[i+1];
+            numero.splice(i,2,resposta);
+            operador.splice(i,1);
             i=-1;
         }
         
     }
-    
-    for(var i = 0; i < numerosMulDiv.length;i++){
-        o++;
-        retorno[o] = numerosMulDiv[i];
-        o++;
-        retorno[o] = operadoresMulDiv[i];
-    }
-    retorno.pop();
-    return retorno;
-}
-function calcularPosNeg(){
-    var valorRetornado = calcularMulDiv();
 
-    var matematica = {
-        '+': function (x, y) { return parseFloat(x) + parseFloat(y) },
-        '-': function (x, y) { return x - y }
-    }; //console.log(matematica['+'](x, y));
-
-    for(var i=0;i<=valorRetornado.length;i++){
-        if(valorRetornado[i]=="+"){
-            let indice = i;
-            let resposta = matematica['+'](valorRetornado[i-1], valorRetornado[i+1]);
-            valorRetornado.splice(i-1,3,String(resposta));
+    for(var i=0;i<=numero.length;i++){
+        if(operador[i] === "+"){
+            let resposta = numero[i] + numero[i+1];
+            numero.splice(i,2,resposta);
+            operador.splice(i,1);
+            i=-1;
         }
-        if(valorRetornado[i]=="-"){
-            let indice = i;
-            let resposta = matematica['-'](valorRetornado[indice-1], valorRetornado[indice+1]);
-            valorRetornado.splice(indice-1,3,String(resposta));
-        }
+        if(operador[i] === "-"){
+            let resposta = numero[i] - numero[i+1];
+            numero.splice(i,2,resposta);
+            operador.splice(i,1);
+            i=-1;
+		}
     }
-    
-    return valorRetornado;
-}
-function igual(){
-    var resultadoFinal = calcularPosNeg();
-	document.getElementById('resultado').value = resultadoFinal;
+
+    return numero;	
 }
